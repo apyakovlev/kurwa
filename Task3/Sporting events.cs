09baac839a3e09f40fdbfc06906be45219153f55
Yakovlev <ap.yakovlev@yandex.ru>
2016-01-13 14:39:34 +0300

    punkt4

---
+++ Task3/Sporting events.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Sporting_events
+    {
+        public string sport { get; set; }
+
+        public int data { get; set; }
+
+        public string mesto { get; set; }
+
+        public int bileti { get; set; }
+
+    }
+}
---
+++ Task3/University.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class University
+    {
+        public string student { get; set; }
+
+        public int data { get; set; }
+
+        public int ball { get; set; }
+    }
+}
